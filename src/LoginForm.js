cc9a5cf79face29ee6a4e16f406e6e332ae53d93
vasiliv <32122468+vasiliv@users.noreply.github.com>
2024-03-15 17:59:05 +0400

    2-carsData

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,4 +1,3 @@
-// LoginForm.js
 import React, { useState } from "react";
 import { login } from "./authService";
 
---
+++ src/carsData.js
@@ -0,0 +1,49 @@
+const carsData = [
+    {
+      id: 1,
+      brand: "Toyota",
+      model: "Corolla",
+      year: 2020,
+      color: "Silver",
+      price: 20000,
+      image: "1.jpg"
+    },
+    {
+      id: 2,
+      brand: "Honda",
+      model: "Civic",
+      year: 2019,
+      color: "Black",
+      price: 22000,
+      image: "2.jpg"
+    },
+    {
+      id: 3,
+      brand: "Ford",
+      model: "Mustang",
+      year: 2021,
+      color: "Red",
+      price: 35000,
+      image: "3.jpg"
+    },
+    {
+      id: 4,
+      brand: "Tesla",
+      model: "Model 3",
+      year: 2022,
+      color: "White",
+      price: 45000,
+      image: "4.jpg"
+    },
+    {
+      id: 5,
+      brand: "Chevrolet",
+      model: "Camaro",
+      year: 2018,
+      color: "Blue",
+      price: 30000,
+      image: "5.jpg"
+    }
+  ];
+  
+  export default carsData;
